6cda9f491857067ae1d922ef8d0ef577eb5d0fb4
Mary Vitoria <maryvitoria054@gmail.com>
2025-08-24 21:27:11 -0300

    - Adicionarei.gitignore para excluir arquivos de ambiente virtual. - Atualizei task02.py para incluir funcionalidade de GPS e alterar comandos de drone. - Criei taskpraeuver1.py para uma nova manobra de drone com tratamento de erros e pra testar as outras funções

--- taskpraeuver1.py
+++ taskpraeuver1.py
@@ -1,47 +1,51 @@
 from app.drone.Drone import Drone
-# import floripa.Camera as measure
+import time
 from app.drone.tools.GPS import GPS
-
-drone = Drone()
 gps = GPS()
 
+
+
+#Instanciando a classe
+
+drone = Drone()
+
 if not drone.connected():
-    print("EI man the drone não ta conectado")
+    print("O drone não conectado veyr")
     exit()
+
+#Definindo uma função
 
 def run():
     try:
         lat, lon, _ = drone.get_gps_position()
         drone.set_home(lat, lon)
         drone.change_to_guided_mode()
-
         drone.arm_drone()
-
-        drone.takeoff(2)
-
-        new_lat, new_lon = gps.meters_to_geo(lat, lon, 2, 90)
+        drone.takeoff(5)
+        time.sleep(5)
+        new_lat, new_lon = gps.meters_to_geo(lat, lon, 4, 180)
         drone.move_to_position(new_lat, new_lon)
-        # measure.wait_to_continue(drone.current_altitude())
-        drone.return_to_home()
+        drone.descend(3)
+        time.sleep(5)
+        drone.land()
+        drone.disarm()
+
+     #Se a gente parar com algum comando no teclado tipo ctrl + c aí o drone pousa
 
     except KeyboardInterrupt:
         drone.land()
         drone.disarm()
         exit()
 
+    #Se der algum erro ou interrupção além da ultima ai mostra o erro e pousa
+
     except Exception as e:
         print(e)
         drone.land()
         drone.disarm()
+        exit()
 
-#while True:
-#    msg = drone.conn.recv_match(type=['RC_CHANNELS', 'RC_CHANNELS_RAW'], blocking=True)
-#    if msg.chan6_raw:
-#        if msg.chan6_raw >= 2014:
-#            print("Canal 6 detectado.")
-#            run()
-#            break
+#Rodando as funções
 
 run()
-
 
